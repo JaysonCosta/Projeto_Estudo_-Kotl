039833b8e6c1316ba8a058606c46096bfcb5827d
jayson.costa <jayson.costa@gmail.com>
2022-06-23 21:32:10 -0300

    Feito Api Endpoint Rest em Go

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe(":8080", nil)
+}
 
-	println("Hello World")
-
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 }
